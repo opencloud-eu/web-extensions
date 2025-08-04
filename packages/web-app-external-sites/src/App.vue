1a16be07553571a5650864559a84e843e8b4c7ec
Dominik Schmidt <dev@dominik-schmidt.de>
2025-08-04 14:01:37 +0200

    feat: enable type checks in external-sites app

--- packages/web-app-external-sites/src/App.vue
+++ packages/web-app-external-sites/src/App.vue
@@ -5,12 +5,20 @@
 </template>
 <script lang="ts">
 import { defineComponent } from 'vue'
+import { useGettext } from 'vue3-gettext'
 
 export default defineComponent({
   name: 'ExternalSite',
   props: {
     name: { type: String, required: true },
     url: { type: String, required: true }
+  },
+  setup() {
+    const { $gettext } = useGettext()
+
+    return {
+      $gettext
+    }
   }
 })
 </script>
