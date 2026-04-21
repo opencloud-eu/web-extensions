af1db400335f18a42f18e8c61fc56328adde0bd0
Jannik Stehle <j.stehle@opencloud.eu>
2026-04-21 08:43:07 +0200

    chore: remove unneeded extension-sdk type declaration

--- packages/web-app-arcade/extension.d.ts
+++ packages/web-app-arcade/extension.d.ts
@@ -1,7 +1,1 @@
 /// <reference types="vite/client" />
-
-// FIXME: remove when extension-sdk provides its own types
-declare module '@opencloud-eu/extension-sdk' {
-  const defineConfig: (config: any) => void
-  export { defineConfig }
-}
