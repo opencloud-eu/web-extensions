1655e198ee62d852aae16d1aaad903bc4fb6e980
Alexander Ackermann <abackermann91@gmail.com>
2026-08-19 11:25:37 +0200

    change: remove drawio from menuitems

--- packages/web-app-draw-io/src/index.ts
+++ packages/web-app-draw-io/src/index.ts
@@ -1,26 +1,18 @@
-import { Resource, urlJoin } from '@opencloud-eu/web-client'
+import { Resource } from '@opencloud-eu/web-client'
 import {
   ApplicationInformation,
-  AppMenuItemExtension,
   AppWrapperRoute,
-  defineWebApplication,
-  useUserStore,
-  useOpenEmptyEditor,
-  useSpacesStore
+  defineWebApplication
 } from '@opencloud-eu/web-pkg'
 import translations from '../l10n/translations.json'
 import App from './App.vue'
 import { useGettext } from 'vue3-gettext'
-import { computed } from 'vue'
 
 const applicationId = 'draw-io'
 
 export default defineWebApplication({
   setup() {
     const { $gettext } = useGettext()
-    const userStore = useUserStore()
-    const { openEmptyEditor } = useOpenEmptyEditor()
-    const spacesStore = useSpacesStore()
 
     const routes = [
       {
@@ -62,30 +54,10 @@ export default defineWebApplication({
       ]
     }
 
-    const menuItems = computed<AppMenuItemExtension[]>(() => {
-      const items: AppMenuItemExtension[] = []
-
-      if (userStore.user && spacesStore.personalSpace) {
-        items.push({
-          id: `app.${appInfo.id}.menuItem`,
-          type: 'appMenuItem',
-          label: () => appInfo.name,
-          color: appInfo.color,
-          icon: appInfo.icon,
-          priority: 30,
-          path: urlJoin(appInfo.id),
-          handler: () => openEmptyEditor(appInfo.id, appInfo.defaultExtension)
-        })
-      }
-
-      return items
-    })
-
     return {
       appInfo,
       routes,
-      translations,
-      extensions: menuItems
+      translations
     }
   }
 })
